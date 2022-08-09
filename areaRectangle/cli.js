5750c69f83fb82617e324544bf2beb0bfc154ec8
Jeremy Pena <5196790+jerpena@users.noreply.github.com>
2022-08-09 15:18:12 -0500

    complete areaRectangle challenges 1-2

--- areaRectangle/cli.js
+++ areaRectangle/cli.js
@@ -1,22 +1,72 @@
-import * as readline from 'node:readline/promises'
-import {stdin as input, stdout as output } from 'node:process'
-const rl = readline.createInterface({input, output})
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+const rl = readline.createInterface( { input, output } );
 
-const CONVERSION_FACTOR = 0.09290304
+const FT2_TO_METER2 = 0.09290304;
+const METER2_TO_FT2 = 10.76391042;
+const userInput = {
+    unit: null,
+    length: null,
+    width: null,
+    areaFt: null,
+    areaM: null,
+}
 
-let length = await rl.question('What is the length of the room in feet? ')
-let width = await rl.question('What is the width of the room in feet? ')
-length = Number(length)
-width = Number(width)
+const getInput = async (userInput) => {
+    const isInputValid = input => input > 0 && !isNaN(input)
 
+    do
+    {
+        userInput.unit = await rl.question( `Choose input unit. (f)Feet (m)Meters : ` );
+    } while ( userInput.unit != 'f' && userInput.unit != 'm' )
+
+    do
+    {
+        userInput.length = Number( await rl.question( 'What is the length of the room? ' ) );
+    }while ( !isInputValid(userInput.length) )
+
+    do
+    {
+        userInput.width = Number( await rl.question( 'What is the width of the room? ' ) );
+    }while ( !isInputValid(userInput.width))
+
+    return userInput
+};
+
+
+const feetToMeters = ft => ft * FT2_TO_METER2;
+const metersToFeet = m => m * METER2_TO_FT2;
+const roundToDecimal = ( num, place = 2 ) => {
+    const deci = Math.pow( 10, place );
+    return Math.round( ( num + Number.EPSILON ) * deci ) / deci;
+};
+
+const calculateArea = input => {
+    const area = input.length * input.width
+
+    if ( input.unit === 'f' )
+    {
+        input.areaFt = area
+        input.areaM = roundToDecimal(feetToMeters(area), 3)
+    } else
+    {
+        input.areaM = area
+        input.areaFt = roundToDecimal(metersToFeet(area), 3)
+    }
+
+    return input;
+};
+
+await getInput(userInput);
 rl.close();
 
-const areaFt = length * width
-let areaM = areaFt * CONVERSION_FACTOR
-areaM = Math.round((areaM + Number.EPSILON) * 1000) / 1000;
+calculateArea(userInput);
+
+const {length, width, unit, areaFt, areaM} = userInput;
+
 const answer = `
 
-You entered dimensions of ${length} by ${width} feet.
+You entered dimensions of ${length} by ${width} ${unit === 'f' ? 'feet' : 'meters'}.
 
 The area is
 
@@ -24,4 +74,4 @@ ${areaFt} square feet
 ${areaM} square meters.
 `
 
-console.log(answer)
+console.log( answer );
